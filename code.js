922316b4918c2d83e74d8998c73cea57d14c5ddd
angi71 <34031103+angi71@users.noreply.github.com>
2020-09-10 20:15:37 +0000

    Update code.js

--- code.js
+++ code.js
@@ -1,64 +1,7 @@
-function createNode(element) {
-    return document.createElement(element);
+// Get the button, and when the user clicks on it, execute myFunction
+document.getElementById("myBtn").onclick = function() {myFunction()};
+
+/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
 }
-listinn = [];
-let efni = document.getElementById('efni')
-
-//næ í conserts api-ið 
-fetch('https://apis.is/concerts')
-    .then((resp) => resp.json())
-    .then(function(data) {
-        let tonleikar = data.results; 
-        // nota map til að fara gegnum gögninn
-        return tonleikar.map(function(tonleika) {
-            // bý til nodinn
-            let li = document.createElement("li"),
-                img = document.createElement("img"),
-                nafn = document.createElement("span");
-                stadur =document.createElement("span");
-                timi = document.createElement("span");
-            // sit gögninn í  nodinn
-            img.src = tonleika.imageSource;
-            nafn.innerHTML = tonleika.eventDateName;
-            stadur.innerHTML = tonleika.eventHallName;
-            timi.innerHTML = tonleika.dateOfShow;
-            //sit nodinn í html skrána 
-            li.append(img);
-            li.append(nafn);
-            li.append(stadur);
-            li.append(timi);
-            efni.append(li);
-            // sit nodinn í object
-            listinn.push({
-                tónleika:tonleika,
-                li:li,
-                nafn:nafn,
-                stadur:stadur,
-                timi:timi
-            });
-        }) 
-    })
-
-    .catch(function(err) {
-        console.log('Fetch Error :-S', err);
-    });
-
-
-const leit = document.forms['leita'].querySelector('input');
-
-// set eventlistener á leitar kassan
-leit.addEventListener('keyup', function(e){
-    // breyti textan í lágstafi
-    const ord = e.target.value.toLowerCase();
-    // fer í gegnum listan sem nodinn eru í
-    Array.from(listinn).forEach(function(listinn){
-        let nafn = listinn.nafn.textContent;
-        // gá hvort stafinir í leitar kassanum eru í nafn sýningar
-        if (nafn.toLowerCase().indexOf(ord)!=-1){
-            listinn.li.style.display = 'grid';
-        }
-        else{
-            listinn.li.style.display = 'none'
-        }
-    })
-})
